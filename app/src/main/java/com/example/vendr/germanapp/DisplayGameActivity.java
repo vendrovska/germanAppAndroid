70388392c0adea04353fe3c4dba9138e1f42c375
Tania Leonova-Vendrovska <Tania Leonova-Vendrovska>
2017-05-27 01:23:09 -0700

    first public commit

---
+++ app/src/main/java/com/example/vendr/germanapp/DisplayGameActivity.java
@@ -0,0 +1,27 @@
+package com.example.vendr.germanapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class DisplayGameActivity extends AppCompatActivity {
+
+    String [] wordsDictionary = new String[]{"one", "two", "three"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_game);
+    }
+    public void setCurrentWordValue(View view){
+        String message = wordsDictionary[0];
+       TextView textview_currentWord = (TextView) findViewById(R.id.textView);
+        textview_currentWord.setText(message);
+    }
+    public void checkUserAnswer(View view){
+        String message = ((Button) view).getText().toString();
+        TextView textview_currentWord = (TextView) findViewById(R.id.textView);
+        textview_currentWord.setText(message);
+    }
+}
