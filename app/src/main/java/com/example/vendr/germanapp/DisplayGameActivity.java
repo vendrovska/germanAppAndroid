dd58c845393d8e7829565fc55bbf3c5852c58ba3
Tania Leonova-Vendrovska <Tania Leonova-Vendrovska>
2017-06-16 16:17:46 -0700

    words now have score bar, bug related to the last word in a dictionary crush fixed

--- app/src/main/java/com/example/vendr/germanapp/DisplayGameActivity.java
+++ app/src/main/java/com/example/vendr/germanapp/DisplayGameActivity.java
@@ -6,6 +6,7 @@ import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.media.Image;
 import android.speech.tts.TextToSpeech;
+import android.speech.tts.UtteranceProgressListener;
 import android.support.constraint.solver.SolverVariable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,6 +16,7 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import org.json.JSONArray;
@@ -27,6 +29,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Locale;
 import java.util.Random;
 
@@ -45,12 +48,14 @@ public class DisplayGameActivity extends AppCompatActivity  implements TextToSpe
     private TextToSpeech tts;
     private Button btnSpeak;
     private TextView txtTeext;
+    RatingBar currentWordScore;
 
     public void getFullGermanDictionary(){
         String s = "";
         try {
             AssetManager assetManager = getAssets();
-            InputStream inputStream = assetManager.open("GermanNounsDictionary.json");
+            //InputStream inputStream = assetManager.open("GermanNounsDictionary.json");
+            InputStream inputStream = assetManager.open("TestDictionary.json");
             InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
             BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
             StringBuilder total = new StringBuilder();
@@ -70,8 +75,6 @@ public class DisplayGameActivity extends AppCompatActivity  implements TextToSpe
     }
     public void fillLocalDictionary(){
         getFullGermanDictionary();
-        //TODO get data from real dictionary
-        //todo:replace code below with a real data
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -83,49 +86,73 @@ public class DisplayGameActivity extends AppCompatActivity  implements TextToSpe
         tts = new TextToSpeech(this, this);
         updateNounTextView();
     }
-    public void checkUserAnswer(View view){
+    public void checkUserAnswer(final View view){
         String articleChosen = ((Button) view).getText().toString();
-        boolean correrctAnswer = articleChosen.equals(currentCorrectArticle);
+        final boolean correrctAnswer = articleChosen.equals(currentCorrectArticle);
         boolean reset = false;
         updateButtonColor(view, correrctAnswer, reset);
-        startTalkToSpeech(view);
-       if(correrctAnswer){
-           currentWord.UserAnswerCount++;
-           if(currentWord.UserAnswerCount >= 3){
-               //remove from wordsDictionary
-               wordsDictionaryList.remove(curIndex); //todo: replace with more efficient structure (array with first and last indexes
+        class TtsCompletionHandler extends UtteranceProgressListener {
+            @Override
+            public void onDone (String utteranceId){
+                // Executing updates on UI (initial) thread
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        updateButtonColor(view, correrctAnswer, true /* reset */);
+                        if(correrctAnswer){
+                            currentWord.UserAnswerCount++;
+                            if(currentWord.UserAnswerCount >= 3){
+                                //remove from wordsDictionary
+                                wordsDictionaryList.remove(curIndex); //todo: replace with more efficient structure (array with first and last indexes
 
-               if(wordsDictionaryList.size()== 0){
-                   openGameEndActivity(view);
-               }
-               if(wordsDictionaryList.size() <= MIN_ROUND_SIZE && !dataSourceIsEmpty){
-                   getRangeFromDictionary();
-               }
-               userScore++;
-           }
-            //add answer to total # of corretct answers
-            //say the word aloud
-            //check if word has article chosen correctly 3 times in a row
-            //remove the word from array if it was guessed three times in a row
-            //check if more words needed to the array (if fewer words are left)
-            //make correct button to be of a green color
-            //OR
-            //clear word correct answers conunting back to 0
-            //proceed to the next word
-            //color pressed button to the red color
+                                 if(wordsDictionaryList.size() <= MIN_ROUND_SIZE && !dataSourceIsEmpty){
+                                    getRangeFromDictionary();
+                                }
+                                userScore++;
+                            }
+                            //add answer to total # of corretct answers
+                            //say the word aloud
+                            //check if word has article chosen correctly 3 times in a row
+                            //remove the word from array if it was guessed three times in a row
+                            //check if more words needed to the array (if fewer words are left)
+                            //make correct button to be of a green color
+                            //OR
+                            //clear word correct answers conunting back to 0
+                            //proceed to the next word
+                            //color pressed button to the red color
+                        }
+                        else {
+                            currentWord.UserAnswerCount = 0;
+                        }
+
+                        if(wordsDictionaryList.size()== 0){
+                            openGameEndActivity(view);
+                        }
+                        else{
+                            updateNounTextView();
+                        }
+
+                    }
+                });
+                //TODO: add stars to reflect user answers for each word
+
+            }
+            @Override
+            public void onError(String utteranceId){
+
+            }
+            @Override
+            public void onStart (String utteranceId){
+
+            }
         }
-       else {
-           currentWord.UserAnswerCount = 0;
-       }
-       updateNounTextView();
-        reset = true;
-        updateButtonColor(view, correrctAnswer, reset);
-        //todo switch fr btn name to handkle diff. article values
-        //todo connect to mongo db dictionary
+
+
+        startTalkToSpeech(new TtsCompletionHandler());
     }
 
-    private void startTalkToSpeech(View view) {
-        speakOut();
+    private void startTalkToSpeech(UtteranceProgressListener listener) {
+        speakOut(listener);
     }
 
     private void updateButtonColor(View v, boolean answerCorrect, boolean reset) {
@@ -157,21 +184,19 @@ public class DisplayGameActivity extends AppCompatActivity  implements TextToSpe
     public void updateNounTextView(){
         //generate random number for picking a word from dictionary
         Random rand = new Random();
-        int curIndex = rand.nextInt(wordsDictionaryList.size());
+        curIndex = rand.nextInt(wordsDictionaryList.size());
         //remember a current word for future refferences
         currentWord = wordsDictionaryList.get(curIndex);
         String message = currentWord.GermanText;
         currentCorrectArticle = currentWord.Article.toUpperCase();
         TextView textview_currentWordGerman = (TextView) findViewById(R.id.textView);
         TextView textview_currentWordEnglish = (TextView) findViewById(R.id.textView2);
-            if(true){
-            //TODO: update the if statement and open "the end" activity
-        }
         textview_currentWordGerman.setText(currentWord.GermanText);
         textview_currentWordEnglish.setText(currentWord.EnglishTranslation);
-
         TextView textview_currentScore = (TextView) findViewById(R.id.textView3);
         textview_currentScore.setText(String.valueOf(userScore));
+        currentWordScore = (RatingBar) findViewById(R.id.nounRatingBar);
+        currentWordScore.setRating(currentWord.UserAnswerCount);
 
     }
     private void establishTextToSpeech(){
@@ -227,12 +252,15 @@ public class DisplayGameActivity extends AppCompatActivity  implements TextToSpe
         Log.e("TTS", "initialization failed");
     }
     }
-    private void speakOut() {
+    private void speakOut(UtteranceProgressListener listener) {
         if(!mutedTts){
             String text = currentWord.GermanWithArticle; //txtTeext.getText().toString();
-            tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
-            while (tts.isSpeaking() ) {
-            };
+            tts.setOnUtteranceProgressListener(listener);
+           // tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+            HashMap<String, String> map = new HashMap<String, String>();
+            map.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID,"messageID");
+            tts.speak(text, TextToSpeech.QUEUE_FLUSH, map);
+
         }
 
     }
@@ -249,4 +277,6 @@ public class DisplayGameActivity extends AppCompatActivity  implements TextToSpe
         ImageButton buttonOff=(ImageButton)findViewById(R.id.muteOff);
         buttonOff.setVisibility(View.VISIBLE);
     }
+
+
 }
