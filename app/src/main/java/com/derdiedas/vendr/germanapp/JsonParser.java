103c86c419f49f9eb478b616576c35511e47bb1f
vendrovska <vendrovska@users.noreply.github.com>
2017-07-03 11:23:38 -0700

    Fixed whitespaces

--- app/src/main/java/com/derdiedas/vendr/germanapp/JsonParser.java
+++ app/src/main/java/com/derdiedas/vendr/germanapp/JsonParser.java
@@ -2,15 +2,15 @@ package com.derdiedas.vendr.germanapp;
 
 
 import java.io.File;
-        import java.io.FileInputStream;
-        import java.io.InputStream;
-        import org.apache.commons.io.IOUtils;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import org.apache.commons.io.IOUtils;
+import org.json.JSONObject;
 
-        import org.json.JSONObject;
 public class JsonParser {
     public static void main(String[] args) throws Exception {
         File f = new File("GermanNounDictionary.json");
-        if (f.exists()){
+        if (f.exists()) {
             InputStream is = new FileInputStream("GermanNounDictionary.json");
             String jsonTxt = IOUtils.toString(is);
             JSONObject json = new JSONObject(jsonTxt);
